c66cc5b00949e83c5b0c815eb15628c348a90aef
Aaron Wolbach <aaron.wolbach@auth0.com>
2021-12-07 14:12:55 -0500

    fix token views

--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -3,28 +3,61 @@
   <v-card elevation="2" outlined class="ma-3 pa-3">
     <v-card-title>Identity Token</v-card-title>
     <v-divider></v-divider>
-    <pre>{{ userJSON | pretty }}</pre>
 
-    <!-- <highlightjs
-      autodetect 
-      :code="JSON.stringify($auth.user, null, 2)" 
-      class="rounded w-100" 
-      v-if="showIDToken"
-    /> -->
+    <v-row>
+      <v-col cols="6">
+        <code class="token" v-if="showIDToken">
+          <span class="header">{{ idToken.header }}</span>
+          <span>.</span>
+          <span class="body">{{ idToken.body }}</span>
+          <span>.</span>
+          <span class="signature">{{ idToken.signature }}</span>
+        </code>
+      </v-col>
+
+      <v-col cols="6">
+        <pre class="json" v-if="showIDToken">{{ userJSON | pretty }}</pre>
+      </v-col>
+    </v-row>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="blue darken-1" outlined @click="showIDToken = !showIDToken">
+        {{ showIDToken ? 'Hide Token' : 'Show Token' }}
+      </v-btn>
+    </v-card-actions>
   </v-card>
 
   <v-card elevation="2" outlined class="ma-3 pa-3">
     <v-card-title>Access Token</v-card-title>
     <v-divider></v-divider>
-    <!-- <textarea v-model="tokenJSON" rows="8" cols="40"></textarea> -->
-    <pre>{{ tokenJSON | pretty }}</pre>
+    
+    <v-row>
+      <v-col cols="6" >
+        <code class="token" v-if="showAccessToken">
+          <span class="header">{{ accessToken.header }}</span>
+          <span>.</span>
+          <span class="body">{{ accessToken.body }}</span>
+          <span>.</span>
+          <span class="signature">{{ accessToken.signature }}</span>
+        </code>
+      </v-col>
 
-    <!-- <highlightjs 
-      autodetect 
-      :code="JSON.stringify(accessToken, null, 2)" 
-      class="rounded w-100"
-      v-if="showAccessToken"
-    /> -->
+      <v-col cols="6">
+        <pre class="json" v-if="showAccessToken">{{ tokenJSON | pretty }}</pre>
+      </v-col>
+    </v-row>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="blue darken-1" outlined @click="showAccessToken = !showAccessToken">
+        {{ showAccessToken ? 'Hide Token' : 'Show Token' }}
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </div>
 </template>
@@ -37,8 +70,10 @@ export default {
       title: 'Debug',
       showIDToken: true,
       showAccessToken: true,
+      idToken: '',
       user: {},
-      accessToken: {}
+      accessToken: '',
+      accessTokenDecoded: {}
     }
   },
   computed: {
@@ -46,7 +81,7 @@ export default {
       return JSON.stringify(this.user)
     },
     tokenJSON () {
-      return JSON.stringify(this.accessToken)
+      return JSON.stringify(this.accessTokenDecoded)
     }
   },
   filters: {
@@ -55,10 +90,26 @@ export default {
     }
   },
   async beforeMount() {
+    // get the raw id token
+    const claims = await this.$auth.getIdTokenClaims()
+    this.idToken = {
+      header: claims.__raw.split('.')[0],
+      body: claims.__raw.split('.')[1],
+      signature: claims.__raw.split('.')[2]
+    }
+    // decode the id token
     this.user = this.$auth.user
+
+    // get the raw access token
     const accesstoken  = await this.$auth.getTokenSilently()
-    this.accessToken = parseJwt(accesstoken)
-        
+    this.accessToken = {
+      header: accesstoken.split('.')[0],
+      body: accesstoken.split('.')[1],
+      signature: accesstoken.split('.')[2]
+    }
+    // decode the access token
+    this.accessTokenDecoded = parseJwt(accesstoken)
+    
     function parseJwt(token) {
       const base64Payload = token.split('.')[1]
       const payload = Buffer.from(base64Payload, 'base64')
@@ -67,3 +118,34 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  pre.json {
+    font-family: monospace, monospace;
+    display: block;
+    overflow: scroll;
+    padding: 0.5em;
+    color: white;
+    background: #282a36;
+  }
+
+  code.token {
+    font-family: monospace, monospace;
+    display: block;
+    overflow-wrap: break-word;
+    padding: 0.5em;
+    color: white;
+    background: #282a36;
+    /* background-color: var(--v-background-base); */
+  }
+
+  code.token > span.header {
+    color: red;
+  }
+  code.token > span.body {
+    color: green;
+  }
+  code.token > span.signature {
+    color: steelblue;
+  }
+</style>

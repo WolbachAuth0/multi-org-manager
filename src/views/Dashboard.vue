2a9c22275e3b1d89e86f12d77243a3d73fe094f2
Aaron Wolbach <aaron.wolbach@auth0.com>
2021-12-13 09:47:56 -0500

    list members

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -9,7 +9,7 @@
     <v-row>
       <v-col cols="2">
         <v-avatar color="primary">
-          <v-icon x-large>{{ mdiAccountCircle }}</v-icon>
+          <v-icon x-large>{{ alert.icon }}</v-icon>
         </v-avatar>
       </v-col>
       <v-col cols="10">
@@ -22,6 +22,13 @@
     <v-card-title>
       {{ title }}
     </v-card-title>
+    
+    <ul>
+      <li v-for="member of members" :key="member.user_id">
+        {{ member.name }}
+      </li>
+    </ul>
+
 
   </v-card>
 </div>
@@ -58,10 +65,18 @@ export default {
     }
     const org = await this.fetchOrg()
     const members = await this.fetchOrgMembers()
+    this.org = org.data
+    this.members = members.data
     console.log(org, members)
-    if (!(org.data.success && members.data.success) ) {
+    if (org.success && members.success) {
+      this.alert.title = 'Success !'
+      this.alert.text = 'We fetched the data from the API.'
+      this.alert.icon = mdiCloudCheckOutline
+      this.alert.visible = true
+    } else {
       this.alert.title = 'Warning !'
       this.alert.text = 'An Error occurred while access http resources.'
+      this.alert.color = 'red'
       this.alert.icon = mdiCloudAlert
       this.alert.visible = true
     }

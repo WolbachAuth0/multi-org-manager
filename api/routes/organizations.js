c36082ed99e5036904748c180bf57920600de378
Aaron Wolbach <aaron.wolbach@auth0.com>
2021-12-15 16:56:50 -0500

    get all orgs endpoint

--- api/routes/organizations.js
+++ api/routes/organizations.js
@@ -12,6 +12,62 @@ const management = require('./../models/management')(scopes)
 
 module.exports = router
 
+/**
+ * List Organizations
+ */
+router
+  .route('/')
+  .get(async (req, res) => {
+    let status = 200
+    let message = ``
+    let data = []
+    try {
+      const options = {
+        per_page: parseInt(req.query.limit) || 50,
+        page: parseInt(req.query.page) || 0
+      }
+      data = await management.organizations.getAll(options)
+      message = `Found ${data.length} organizations`
+    } catch (error) {
+      status = parseInt(error.statusCode) || 500
+      message = error.message
+      data = error
+    }
+
+    const json = responseFormatter(req, res, { status, message, data })
+    res.status(status).json(json)
+  })
+
+/**
+ * Get organization by ID
+ */
+router
+  // .all(verifyJWT)
+  .route('/:org_id')
+  .get(
+    // checkJWTScopes(['read:organizations'], options),
+    async (req, res) => {
+      const id = req.params.org_id
+      let status = 200
+      let message = ``
+      let data = []
+      try {
+        data = await management.organizations.getByID({ id })
+        message = `Found details for "${data.display_name}".`
+      } catch (error) {
+        status = parseInt(error.statusCode) || 500
+        message = error.message
+        data = error
+      }
+      
+      const json = responseFormatter(req, res, { status, message, data })
+      res.status(status).json(json)
+    }
+  )
+
+/**
+ * List Organization Members
+ */
 router
   // .all(verifyJWT)
   .route('/:org_id/members')
@@ -25,30 +81,6 @@ router
       try {
         data = await management.organizations.getMembers({ id })
         message = `Found ${data.length} members of organization.`
-      } catch (error) {
-        status = parseInt(error.statusCode) || 500
-        message = error.message
-        data = error
-      }
-      
-      const json = responseFormatter(req, res, { status, message, data })
-      res.status(status).json(json)
-    }
-  )
-
-  router
-  // .all(verifyJWT)
-  .route('/:org_id')
-  .get(
-    // checkJWTScopes(['read:organizations'], options),
-    async (req, res) => {
-      const id = req.params.org_id
-      let status = 200
-      let message = ``
-      let data = []
-      try {
-        data = await management.organizations.getByID({ id })
-        message = `Found details for "${data.display_name}".`
       } catch (error) {
         status = parseInt(error.statusCode) || 500
         message = error.message

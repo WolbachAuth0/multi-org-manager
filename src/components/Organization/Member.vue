c203e74559c541329a337eb4bfd32fc49c7736a4
Aaron Wolbach <aaron.wolbach@auth0.com>
2021-12-29 16:57:55 -0500

    member form working

---
+++ src/components/Organization/Member.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-dialog v-model="isShown">
+    <v-card class="pa-6">
+
+      (TODO: Build form for modifying a member)
+
+      <v-card-actions>
+        <v-btn color="primary" @click="createConnection">
+          Save
+        </v-btn>
+        <v-btn color="error" @click="isShown = false">
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+
+export default {
+  name: 'Member',
+  data () {
+    return {
+      isShown: false
+    }
+  },
+  props: {
+    org: { type: Object },
+    visible: { type: Boolean }
+  },
+  watch: {
+    visible (newValue, oldValue) {
+      this.isShown = newValue
+      console.log('connection: visible changed', newValue, oldValue)
+    },
+    isShown(newValue, oldValue) {
+      if (newValue) {
+        this.$emit('show', true)
+      } else {
+        this.$emit('hide', false)
+      }
+    }
+  },
+  methods: {
+    async createConnection () {
+      console.log('You clicked the button!')
+    },
+  }
+}
+</script>

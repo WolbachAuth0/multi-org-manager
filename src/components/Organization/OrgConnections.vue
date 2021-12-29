d8e11f59fb40e4437db3c6765b5175df891375ef
Aaron Wolbach <aaron.wolbach@auth0.com>
2021-12-29 16:56:33 -0500

    moved connection file

--- src/components/Organization/OrgConnections.vue
+++ src/components/Organization/OrgConnections.vue
@@ -43,7 +43,7 @@
 </template>
 
 <script>
-import Connection from './../Connection.vue'
+import Connection from './Connection.vue'
 import EventBus from './../../helpers/eventBus'
 export default {
   name: 'OrgConnections',

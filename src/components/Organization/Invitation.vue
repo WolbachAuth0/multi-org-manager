7408ec4a4e112112b6bd1c13c88bcc1a9dd69948
Aaron Wolbach <aaron.wolbach@auth0.com>
2022-01-18 17:10:48 -0500

    Invitations working commit

--- src/components/Organization/Invitation.vue
+++ src/components/Organization/Invitation.vue
@@ -16,9 +16,76 @@
           </v-toolbar-items>
         </v-toolbar>
 
-        <v-card-text class="ma-5">
+        <!-- <v-card-text class="ma-5">
           (TODO: Build a form for creating a new invitation)
-        </v-card-text>
+        </v-card-text> -->
+
+        <v-row class="ma-5">
+          <v-col cols="12">
+            <v-card-title>
+              Configure Application
+            </v-card-title>
+
+            <v-select
+              v-model="application.selected"
+              :hint="application.hint"
+              :items="application.items"
+              :item-text="application.itemText"
+              :item-value="application.itemValue"
+              :label="application.label"
+              outlined
+              persistent-hint
+              return-object
+              single-line
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row class="ma-5">
+          <v-card-title>
+            Send Invites by Email
+          </v-card-title>
+          <v-card-text>
+            You must specify an email address to send invitations. Optionally, you may specify the connection 
+            you wish for users to accept the invitation with and the roles they should be assigned.
+          </v-card-text>
+
+          <v-col cols="4">
+            <v-text-field
+              :label="email.label"
+              :placeholder="email.label"
+              outlined
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="4">
+            <v-select
+              v-model="connection.selected"
+              :items="connection.items"
+              :item-text="connection.itemText"
+              :item-value="connection.itemValue"
+              :label="connection.label"
+              :disabled="connection.disabled"
+              outlined
+              return-object
+              single-line
+            ></v-select>
+          </v-col> 
+
+          <v-col cols="4">
+            <v-select
+              v-model="role.selected"
+              :items="role.items"
+              :item-text="role.itemText"
+              :item-value="role.itemValue"
+              :label="role.label"
+              :disabled="role.disabled"
+              outlined
+              return-object
+              single-line
+            ></v-select>
+          </v-col>        
+        </v-row>
 
         <v-card-actions class="ma-5">
           <v-btn color="primary" @click="createInvitation">
@@ -37,7 +104,42 @@ export default {
   name: 'Invitation',
   data () {
     return {
-      isShown: false
+      isShown: false,
+      application: {
+        hint: 'You may select any application that your organization has enabled.',
+        label: 'Your Application',
+        itemText: 'name',
+        itemValue: 'id',
+        selected: { name: '', id: null },
+        items: [
+          { name: 'TODO: Fetch Org-Enabled Applications from API', id: 'get-id'}
+        ]
+      },
+      email: {
+        disabled: true,
+        label: 'Enter email address',
+        itemText: 'email',
+      },
+      connection: {
+        disabled: true,
+        label: 'Select Enabled Connection',
+        itemText: 'name',
+        itemValue: 'id',
+        selected: { name: '', id: null },
+        items: [
+          { name: 'TODO: Fetch Org Connections from API', id: 'get-id'}
+        ]
+      },
+      role: {
+        disabled: true,
+        label: 'Select Roles',
+        itemText: 'name',
+        itemValue: 'id',
+        selected: { name: '', id: null },
+        items: [
+          { name: 'TODO: Fetch Roles from API', id: 'get-id'}
+        ]
+      },
     }
   },
   props: {

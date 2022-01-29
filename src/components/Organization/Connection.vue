80166c84f0fde6d4a3dbaad30c7aa22d4d1ec398
Aaron Wolbach <aaron.wolbach@auth0.com>
2022-01-29 15:09:18 -0500

    add new OIDC connection form

--- src/components/Organization/Connection.vue
+++ src/components/Organization/Connection.vue
@@ -16,9 +16,43 @@
           </v-toolbar-items>
         </v-toolbar>
 
+        <v-card-title>OpenID Connect Connection</v-card-title>
         <v-card-text class="ma-5">
-          (TODO: Create a form for creating a new connection)
+          Instructions for creating an OIDC connection
         </v-card-text>
+
+        <div class="mx-3">
+          <v-text-field v-model="oidc.connectionName.value"
+                        :label="oidc.connectionName.label"
+                        :hint="oidc.connectionName.hint"
+                        outlined
+                        clearable
+                        persistent-hint>
+          </v-text-field>
+          
+          <v-text-field v-model="oidc.issuerURL.value"
+                        :label="oidc.issuerURL.label"
+                        :hint="oidc.issuerURL.hint"
+                        outlined
+                        clearable
+                        persistent-hint>
+          </v-text-field>
+          
+          <v-text-field v-model="oidc.clientID.value"
+                        :label="oidc.clientID.label"
+                        :hint="oidc.clientID.hint"
+                        outlined
+                        clearable
+                        persistent-hint>
+          </v-text-field>
+        </div>
+
+        <v-card-subtitle>Callback URL</v-card-subtitle>
+        <v-card-text>You may need to configure the OIDC Issuer with this callback URL:</v-card-text>
+        <pre>https://science-experiment.us.auth0.com/login/callback</pre>
+
+        <v-switch v-model="oidc.syncUser.value" :label="oidc.syncUser.label"
+        ></v-switch>
 
         <v-card-actions class="ma-5">
           <v-btn color="primary" @click="createConnection">
@@ -37,7 +71,28 @@ export default {
   name: 'Connection',
   data () {
     return {
-      isShown: false
+      isShown: false,
+      oidc: {
+        syncUser: {
+          value: true,
+          label: 'Sync user\'s profile attributes at each login.'
+        },
+        connectionName: {
+          value: '',
+          hint: 'This is a logical identifier of the connection. This name cannot be changed.',
+          label: 'Connection Name',
+        },
+        issuerURL: {
+          value: '',
+          hint: 'Enter the URL of the discovery document of the OpenID Connect provider you want to connect with.',
+          label: 'Issuer URL',
+        },
+        clientID: {
+          value: '',
+          hint: 'Obtaining the Client ID differs across providers. Please check your provider\'s documentation.',
+          label: 'Client ID',
+        }
+      }
     }
   },
   props: {

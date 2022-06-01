100d803883c55037117584326c30538310555c99
Aaron Wolbach <aaron.wolbach@auth0.com>
2022-06-01 11:27:32 -0400

    whitespace

--- api/routes/organizations.js
+++ api/routes/organizations.js
@@ -18,8 +18,8 @@ router
   .get(organizations.list)
 
 router.route('/:org_id')  // https://domain/api/v1/organizations/:org_id
-  .all(verifyJWT)   // checks signature on the access token
-  .all(checkJWTOrgID)   // checks to make sure the access token has organization id scope
+  .all(verifyJWT)         // checks signature on the access token
+  .all(checkJWTOrgID)     // checks to make sure the access token has organization id scope
   .get(
     checkJWTPermissions(['read:organizations']),
     organizations.getByID

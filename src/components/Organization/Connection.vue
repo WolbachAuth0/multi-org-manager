92c8fc9e38e5195f27eab4abfbfe1310407b4c5e
Tyler Keesling <tyler.keesling@okta.com>
2022-09-09 15:22:53 +0000

    Adding the autofill button for connections

--- src/components/Organization/Connection.vue
+++ src/components/Organization/Connection.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-dialog v-model="isShown">
+  <v-dialog width="40%" v-model="isShown">
     <div class="d-flex justify-center">
-      <v-card width="50%">
+      <v-card width="100%">
         <v-toolbar dark color="secondary">
           <v-card-title>
             Enable New Connection
@@ -17,9 +17,7 @@
         </v-toolbar>
 
         <v-card-title>OpenID Connect Connection</v-card-title>
-        <v-card-text class="ma-5">
-          Instructions for creating an OIDC connection
-        </v-card-text>
+        <v-card-subtitle>Instructions for creating an OIDC connection</v-card-subtitle>
 
         <div class="mx-3">
           <v-text-field v-model="oidc.connectionName.value"
@@ -49,14 +47,18 @@
 
         <v-card-subtitle>Callback URL</v-card-subtitle>
         <v-card-text>You may need to configure the OIDC Issuer with this callback URL:</v-card-text>
+        <v-card-text>
         <pre>https://science-experiment.us.auth0.com/login/callback</pre>
+        </v-card-text>
+        <v-switch class="mx-3" v-model="oidc.syncUser.value" :label="oidc.syncUser.label" />
 
-        <v-switch v-model="oidc.syncUser.value" :label="oidc.syncUser.label"
-        ></v-switch>
-
-        <v-card-actions class="ma-5">
+        <v-card-actions class="ma-5 justify-space-between">
           <v-btn color="primary" @click="createConnection">
             Create Connection
+          </v-btn>
+
+          <v-btn color="primary" @click="autofillConnection">
+            Autofill Connection
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -114,15 +116,32 @@ export default {
   },
   methods: {
     async createConnection () {
-      const announcement = {
-        text: 'Creating new connections is not currently supported. Work in progress.',
-        type: 'info',
-        top: true,
-        right: true,
-        left: false
+      const orgID = this.$auth.user.org_id
+      const accesstoken = await this.$auth.getTokenSilently()
+      const { connectionName, issuerURL, clientID } = this.$data.oidc;
+      const newConnection = {
+        name: connectionName.value,
+        strategy: 'oidc',
+        options: {
+          scope: 'openid profile email',
+          client_id: clientID.value,
+          discovery_url: issuerURL.value
+        }
       }
-      EventBus.$emit('announce', announcement)
+      try {
+        const response = await this.$http(accesstoken).post(`/organizations/${orgID}/connections`, newConnection)
+        
+      } catch (err) {
+
+      }
+      
     },
+
+    autofillConnection() {
+      this.$data.oidc.connectionName.value = "just-a-regular-old-plumbus"
+      this.$data.oidc.issuerURL.value = "sso-idp-experiments.us.auth0.com/.well-known/openid-configuration"
+      this.$data.oidc.clientID.value = "6bFUw7nMJdCmSLi5yx8kPuMVaykKumUf"
+    }
   }
 }
 </script>

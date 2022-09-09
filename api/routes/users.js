929183919c0d84731e96d1d268ce3db70f2d7580
WolbachAuth0 <aaron.wolbach@okta.com>
2022-09-09 15:42:53 -0400

    add explanatory comments

--- api/routes/users.js
+++ api/routes/users.js
@@ -12,14 +12,15 @@ function checkJWTUserID (req, res, next) {
   return checkJWTScopes(orgIDs, { customScopeKey, failWithError })(req, res, next)
 }
 
-// Organizations
+// Users
+// http://localhost:8081/api/users/:user_id
 router
   .route('/:user_id')
-  .all(verifyJWT)
-  .all(checkJWTUserID)
+  .all(verifyJWT)                                   // verify signature on access token
+  .all(checkJWTUserID)                              // verifies that the user id in the access token equals user_id in url param
   .get(
-    checkJWTPermissions(['read:user_profile']),
-    users.getById
+    checkJWTPermissions(['read:user_profile']),     // verify access token contains necessary permission(s)
+    users.getById                                   // execute the get User by Id function
   )
   .patch(
     checkJWTPermissions(['update:user_profile']),

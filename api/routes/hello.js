a0e6f357961653554e1794ec546b8dfc18f70d81
WolbachAuth0 <aaron.wolbach@okta.com>
2022-12-05 14:10:21 -0500

    add login route

--- api/routes/hello.js
+++ api/routes/hello.js
@@ -24,3 +24,22 @@ router
       res.status(status).json(json)
     }
   })
+
+/**
+  This endpoint has to be here to redirect invitation links to the signup screen.
+  
+  http://localhost:8081/api/login?invitation=W6yRtawKU1wmi9vY9Y0GbjGSpEQErLvq&organization=org_poSk5O5ljabdHiKV&organization_name=okta
+*/
+router
+  .route('/login')
+  .get((req, res) => {
+    const query = Object.assign(req.query, {
+      response_type: 'code',
+      client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
+      redirect_uri: `${process.env.VUE_APP_DOMAIN}/profile`,
+      response_mode: 'query'
+    })
+    const qs = new URLSearchParams(query).toString()
+    const to = `https://${process.env.VUE_APP_CUSTOM_DOMAIN}/authorize?${qs}`
+    res.redirect(to)
+  })
